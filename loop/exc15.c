57d46c12b0af92921b8a6d80fbd43b29c6eeb485
minhisnothere <AT210335@actvn.edu.vn>
2025-03-12 13:12:26 +0700

    Create exc15.c

---
+++ loop/exc15.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+	int n;
+  double a;
+	printf("nhap vao so tien gui\n");
+	scanf(" %lf",&a);
+	printf("nhap vao so thang gui\n");
+	scanf("%d",&n);
+	printf("%lf", a*pow(1.0045,n));
+}
