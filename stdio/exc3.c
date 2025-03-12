b11d2deaf9971a88a8ead44d7e1afa0f38a672eb
minhisnothere <AT210335@actvn.edu.vn>
2025-03-12 12:37:09 +0700

    Create exc3.c

---
+++ stdio/exc3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	long long x,y,a,b,c,d;
+	scanf("%ld%ld",&x,&y);
+	printf("%ld\n",x+y);
+	printf("%ld\n", x-y);
+	printf ("%ld\n", x*y);
+	printf("%.3lf", (float)x/y);
+}
